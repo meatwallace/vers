meatwallace/campaign

feat(#18): implement initial world creation flow

projects/service-api/src/schema/mutations/delete-world.test.ts
@@ -1,0 +1,57 @@
+import createJWKSMock from 'mock-jwks';
+import { drop } from '@mswjs/data';
+import { createTestAccessToken } from '@chrononomicon/service-test-utils';
+import { env } from '../../env';
+import { createMockGQLContext } from '../../test-utils';
+import { server } from '../../mocks/node';
+import { db } from '../../mocks/db';
+import { deleteWorld } from './delete-world';
+
+const jwks = createJWKSMock(`https://${env.AUTH0_DOMAIN}/`);
+
+test('it deletes a world', async () => {
+  server.use(jwks.handler);
+
+  db.world.create({
+    id: 'test_id',
+  });
+
+  const { accessToken } = createTestAccessToken({
+    jwks,
+    audience: env.API_IDENTIFIER,
+    issuer: `https://${env.AUTH0_DOMAIN}/`,
+  });
+
+  const ctx = createMockGQLContext({ accessToken });
+  const args = {
+    input: {
+      worldID: 'test_id',
+    },
+  };
+
+  const result = await deleteWorld({}, args, ctx);
+
+  expect(result).toMatchObject({
+    success: true,
+  });
+
+  drop(db);
+});
+
+test('it returns an error if the user isnt authenticated', async () => {
+  const ctx = createMockGQLContext({});
+  const args = {
+    input: {
+      worldID: 'test_id',
+    },
+  };
+
+  const result = await deleteWorld({}, args, ctx);
+
+  expect(result).toMatchObject({
+    error: {
+      title: 'Unauthorized',
+      message: 'You must be logged in to perform this action',
+    },
+  });
+});

projects/service-api/src/schema/queries/get-worlds.ts
@@ -1,0 +1,47 @@
+import { Context } from '../../types';
+import { isAuthed } from '../../utils';
+import { builder } from '../builder';
+import { World } from '../types';
+
+type Args = {
+  input: typeof GetWorldsInput.$inferInput;
+};
+
+export async function getWorlds(
+  _: object,
+  args: Args,
+  ctx: Context,
+): Promise<Array<typeof World.$inferType>> {
+  if (!isAuthed(ctx)) {
+    // TODO(#16): capture via sentry
+    throw new Error('Unauthorized');
+  }
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const world = await ctx.services.world.getWorlds({
+      ownerID: ctx.user.id,
+    });
+
+    return world;
+  } catch (error: unknown) {
+    // TODO(#16): capture via Sentry
+    throw error;
+  }
+}
+
+const GetWorldsInput = builder.inputType('GetWorldsInput', {
+  fields: (t) => ({
+    placeholder: t.string({ required: false }),
+  }),
+});
+
+builder.queryField('getWorlds', (t) =>
+  t.field({
+    type: [World],
+    args: {
+      input: t.arg({ type: GetWorldsInput, required: true }),
+    },
+    resolve: getWorlds,
+  }),
+);

projects/service-api/src/services/world-service/update-world.test.ts
@@ -1,0 +1,69 @@
+import createJWKSMock from 'mock-jwks';
+import { drop } from '@mswjs/data';
+import { createTestAccessToken } from '@chrononomicon/service-test-utils';
+import { env } from '../../env';
+import { server } from '../../mocks/node';
+import { db } from '../../mocks/db';
+import { createServiceContext } from '../utils';
+import { updateWorld } from './update-world';
+
+const ISSUER = `https://${env.AUTH0_DOMAIN}/`;
+
+const jwks = createJWKSMock(ISSUER);
+
+test('it updates a world', async () => {
+  db.user.create({
+    id: 'test_id',
+  });
+
+  db.world.create({
+    id: 'test_id',
+    ownerID: 'test_id',
+    name: 'Test World',
+  });
+
+  server.use(jwks.handler);
+
+  const { accessToken } = createTestAccessToken({
+    jwks,
+    audience: env.API_IDENTIFIER,
+    issuer: ISSUER,
+  });
+
+  const ctx = createServiceContext({
+    apiURL: env.WORLDS_SERVICE_URL,
+    accessToken,
+  });
+
+  const args = {
+    ownerID: 'test_id',
+    worldID: 'test_id',
+    name: 'Updated World',
+  };
+
+  const result = await updateWorld(args, ctx);
+
+  expect(result).toMatchObject({
+    id: 'test_id',
+    ownerID: 'test_id',
+    name: 'Updated World',
+    fantasyType: 'Medium',
+    technologyLevel: 'Medieval',
+    archetype: null,
+    atmosphere: 'Neutral',
+    population: 'Average',
+    geographyType: 'Supercontinent',
+    geographyFeatures: [
+      'Deserts',
+      'Forest',
+      'Mountains',
+      'Plains',
+      'Swamps',
+      'Tundra',
+    ],
+    createdAt: expect.any(Date),
+    updatedAt: expect.any(Date),
+  });
+
+  drop(db);
+});

meatwallace/vers

projects/service-api/src/schema/mutations/delete-world.test.ts
@@ -1,0 +1,57 @@
+import { createJWKSMock } from 'mock-jwks';
+import { drop } from '@mswjs/data';
+import { createTestAccessToken } from '@chrononomicon/service-test-utils';
+import { env } from '../../env';
+import { createMockGQLContext } from '../../test-utils';
+import { server } from '../../mocks/node';
+import { db } from '../../mocks/db';
+import { deleteWorld } from './delete-world';
+
+const jwks = createJWKSMock(`https://${env.AUTH0_DOMAIN}/`);
+
+test('it deletes a world', async () => {
+  server.use(jwks.mswHandler);
+
+  db.world.create({
+    id: 'test_id',
+  });
+
+  const { accessToken } = createTestAccessToken({
+    jwks,
+    audience: env.API_IDENTIFIER,
+    issuer: `https://${env.AUTH0_DOMAIN}/`,
+  });
+
+  const ctx = createMockGQLContext({ accessToken });
+  const args = {
+    input: {
+      worldID: 'test_id',
+    },
+  };
+
+  const result = await deleteWorld({}, args, ctx);
+
+  expect(result).toMatchObject({
+    success: true,
+  });
+
+  drop(db);
+});
+
+test('it returns an error if the user isnt authenticated', async () => {
+  const ctx = createMockGQLContext({});
+  const args = {
+    input: {
+      worldID: 'test_id',
+    },
+  };
+
+  const result = await deleteWorld({}, args, ctx);
+
+  expect(result).toMatchObject({
+    error: {
+      title: 'Unauthorized',
+      message: 'You must be logged in to perform this action',
+    },
+  });
+});

projects/service-api/src/schema/queries/get-worlds.ts
@@ -1,0 +1,47 @@
+import { Context } from '../../types';
+import { isAuthed } from '../../utils';
+import { builder } from '../builder';
+import { World } from '../types';
+
+type Args = {
+  input: typeof GetWorldsInput.$inferInput;
+};
+
+export async function getWorlds(
+  _: object,
+  args: Args,
+  ctx: Context,
+): Promise<Array<typeof World.$inferType>> {
+  if (!isAuthed(ctx)) {
+    // TODO(#16): capture via sentry
+    throw new Error('Unauthorized');
+  }
+
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const worlds = await ctx.services.world.getWorlds({
+      ownerID: ctx.user.id,
+    });
+
+    return worlds;
+  } catch (error: unknown) {
+    // TODO(#16): capture via Sentry
+    throw error;
+  }
+}
+
+const GetWorldsInput = builder.inputType('GetWorldsInput', {
+  fields: (t) => ({
+    placeholder: t.string({ required: false }),
+  }),
+});
+
+builder.queryField('getWorlds', (t) =>
+  t.field({
+    type: [World],
+    args: {
+      input: t.arg({ type: GetWorldsInput, required: true }),
+    },
+    resolve: getWorlds,
+  }),
+);

projects/service-api/src/services/world-service/update-world.test.ts
@@ -1,0 +1,69 @@
+import { createJWKSMock } from 'mock-jwks';
+import { drop } from '@mswjs/data';
+import { createTestAccessToken } from '@chrononomicon/service-test-utils';
+import { env } from '../../env';
+import { server } from '../../mocks/node';
+import { db } from '../../mocks/db';
+import { createServiceContext } from '../utils';
+import { updateWorld } from './update-world';
+
+const ISSUER = `https://${env.AUTH0_DOMAIN}/`;
+
+const jwks = createJWKSMock(ISSUER);
+
+test('it updates a world', async () => {
+  db.user.create({
+    id: 'test_id',
+  });
+
+  db.world.create({
+    id: 'test_id',
+    ownerID: 'test_id',
+    name: 'Test World',
+  });
+
+  server.use(jwks.mswHandler);
+
+  const { accessToken } = createTestAccessToken({
+    jwks,
+    audience: env.API_IDENTIFIER,
+    issuer: ISSUER,
+  });
+
+  const ctx = createServiceContext({
+    apiURL: env.WORLDS_SERVICE_URL,
+    accessToken,
+  });
+
+  const args = {
+    ownerID: 'test_id',
+    worldID: 'test_id',
+    name: 'Updated World',
+  };
+
+  const result = await updateWorld(args, ctx);
+
+  expect(result).toMatchObject({
+    id: 'test_id',
+    ownerID: 'test_id',
+    name: 'Updated World',
+    fantasyType: 'Medium',
+    technologyLevel: 'Medieval',
+    archetype: null,
+    atmosphere: 'Neutral',
+    population: 'Average',
+    geographyType: 'Supercontinent',
+    geographyFeatures: [
+      'Deserts',
+      'Forest',
+      'Mountains',
+      'Plains',
+      'Swamps',
+      'Tundra',
+    ],
+    createdAt: expect.any(Date),
+    updatedAt: expect.any(Date),
+  });
+
+  drop(db);
+});