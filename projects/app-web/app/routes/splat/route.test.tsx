15e3cdcb6af8462374c1eb66845f349ffe102956
Geoff Whatley <me@geoffwhatley.com>
2025-03-20 16:42:08 +1100

    feat(#90): add splat route for page not found errors

---
+++ projects/app-web/app/routes/splat/route.test.tsx
@@ -0,0 +1,22 @@
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { createRoutesStub } from 'react-router';
+import { action, ErrorBoundary, loader, NotFound } from './route';
+
+test('it renders the error boundary with a page not found message', async () => {
+  const SplatStub = createRoutesStub([
+    {
+      action,
+      Component: NotFound,
+      ErrorBoundary: ErrorBoundary,
+      loader,
+      path: '*',
+    },
+  ]);
+
+  render(<SplatStub />);
+
+  const errorMessage = await screen.findByText('Page not found');
+
+  expect(errorMessage).toBeInTheDocument();
+});
