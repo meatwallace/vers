8b70d866a4d0c75956df442de975ba99a25f7ffb
Geoff Whatley <me@geoffwhatley.com>
2025-03-26 10:19:27 +1100

    fix(#93): include idle client styles in prod build

--- projects/app-web/panda.config.ts
+++ projects/app-web/panda.config.ts
@@ -3,7 +3,11 @@ import { preset } from '@vers/panda-preset';
 
 export default defineConfig({
   exclude: [],
-  include: ['../lib-design-system/src/**/*.tsx', './app/**/*.{ts,tsx}'],
+  include: [
+    '../lib-design-system/src/**/*.tsx',
+    '../lib-idle-client/src/**/*.tsx',
+    './app/**/*.{ts,tsx}',
+  ],
   jsxFramework: 'react',
   outdir: 'app/styled-system',
   preflight: true,
